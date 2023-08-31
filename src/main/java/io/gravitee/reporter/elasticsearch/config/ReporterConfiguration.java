2d37bf5093b54673453be32b20fc50dad86793f6
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-08-31 10:37:19 +0200

    refactor: drop support for es5 and es6
    BREAKING CHANGE: dropped support for es5 and es6 - needs apim 4.1.x

--- src/main/java/io/gravitee/reporter/elasticsearch/config/ReporterConfiguration.java
+++ src/main/java/io/gravitee/reporter/elasticsearch/config/ReporterConfiguration.java
@@ -494,9 +494,7 @@ public class ReporterConfiguration {
     }
 
     public boolean isProxyConfigured() {
-        return !EnvironmentUtils
-            .getPropertiesStartingWith((ConfigurableEnvironment) environment, "reporters.elasticsearch.http.proxy")
-            .isEmpty();
+        return !EnvironmentUtils.getPropertiesStartingWith(environment, "reporters.elasticsearch.http.proxy").isEmpty();
     }
 
     public String getExtendedSettingsTemplate() {
